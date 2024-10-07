6228b2ef1c32b7225fa21955b90c864911a885b7
priyanshrm <priyanshmahendra@gmail.com>
2024-10-07 17:51:15 +0530

    Exercise 2-7. Write the function rightrot(n, b) which rotates the integer n to the right by b bit positions.

--- test.c
+++ test.c
@@ -3,48 +3,15 @@
 #include <ctype.h>
 #define BUFFER_LENGTH 100
 
-int getStrLength(char arr[])
-{
-    int i = 0;
-    while (arr[i] != '\0' && arr[i] != '\n')
-        i++;
-    return i;
-}
-
-int findIndex(char arr[], char target)
-{
-    int length = getStrLength(arr);
-    for (int i = 0; i < length; i++)
-    {
-        if (arr[i] == target)
-            return i;
-    }
-    return -1;
-}
-
-void deleteChars(char str1[], char str2[])
-{
-    char result[BUFFER_LENGTH];
-    int c = 0;
-    int str1len = getStrLength(str1);
-    for (int i = 0; i < str1len; i++)
-    {
-        if (findIndex(str2, str1[i]) != -1)
-            continue;
-        result[c] = str1[i];
-        c++;
-    }
-    printf("%s", result);
-}
-
 int main()
 {
-    char str1[BUFFER_LENGTH];
-    char str2[BUFFER_LENGTH];
-    printf("Enter a string 1: ");
-    fgets(str1, BUFFER_LENGTH, stdin);
-    printf("Enter a string 2: ");
-    fgets(str2, BUFFER_LENGTH, stdin);
-    deleteChars(str1, str2);
+    int num, b;
+    printf("Enter number: ");
+    scanf("%d", &num);
+    printf("Enter no. of right shift: ");
+    scanf("%d", &b);
+    num = num >> b;
+    printf("after shift: %d", num);
+
     return 0;
 }
