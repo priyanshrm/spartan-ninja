7ee7b225e80038d6bc0af5b801bb41c0a606b541
priyanshrm <priyanshmahendra@gmail.com>
2024-10-19 20:09:02 +0530

    Exercise - Convert string s to float.

--- test.c
+++ test.c
@@ -1,28 +1,52 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #define BUFFER_LENGTH 100
 
-int rIndex(char s[], char t)
+int ctoi(s)
+char s;
 {
-    int slength = strlen(s) - 1;
-    for (int i = slength; i >= 0; i--)
+    return (int)(s) - '0';
+}
+
+float stof(s)
+char s[];
+{
+    int tens = 10;
+    float result = 0.00 - 0.00;
+    bool flag = false;
+    int length = strlen(s) - 1;
+    for (int i = 0; i < length; i++)
     {
-        if (s[i] == t)
-            return (slength - i);
+        if (s[i] == '.')
+        {
+            flag = true;
+            tens = 10;
+            continue;
+        }
+        int num = ctoi(s[i]);
+        if (num > 9 || num < 0)
+        {
+            printf("Invalid string. Cannot convert to float!\n");
+            return -1.00;
+        }
+        if (flag)
+        {
+            result += (float)num / tens;
+            tens *= 10;
+            continue;
+        }
+        result = 10 * result + (float)num;
     }
-    return -1;
+    return result;
 }
 
 int main()
 {
-    char input[BUFFER_LENGTH];
-    char target;
-
-    printf("Enter string: ");
-    fgets(input, BUFFER_LENGTH, stdin);
-    printf("Enter target: ");
-    scanf("%c", &target);
-    printf("%d\n", rIndex(input, target));
+    char alpha[BUFFER_LENGTH];
+    printf("Enter number: ");
+    fgets(alpha, BUFFER_LENGTH, stdin);
+    printf("%f", stof(alpha));
 
     return 0;
 }
